8d5f597c703cb2f8a193689debc7ea2b8d30fd25
Vitor Kato <vitor48kato@gmail.com>
2024-06-21 22:55:27 -0300

    Fixed code, separate files

--- Microservices/inventory_service.py
+++ Microservices/inventory_service.py
@@ -3,16 +3,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 from product import Product
 
 app = Flask(__name__)
 
-DATABASE_URI = 'sqlite///inventory.db'
+DATABASE_URI = 'sqlite:///inventory.db'
 Base = declarative_base()
 engine = create_engine(DATABASE_URI)
 Session = sessionmaker(bind=engine)
 
+
 Base.metadata.create_all(engine)
+
 
 @app.route('/reserve', methods=['POST'])
 def reserve_inventory():
@@ -30,10 +33,10 @@ def reserve_inventory():
 
         product.stock -= quantity
         session.commit()
-        return jsonify({"total_price": product.prize * quantity}), 200
+        return jsonify({"total_price": product.price * quantity}), 200
     except SQLAlchemyError as e:
         session.rollback()
-        return jsonify({"error": str(e)}), 800
+        return jsonify({"error": str(e)}), 400
     finally:
         session.close()
 
--- Microservices/order_service.py
+++ Microservices/order_service.py
@@ -3,9 +3,9 @@ import requests
 
 app = Flask(__name__)
 
-INVENTORY_SERVICE_URL = 'http://localhost:5001'
-PAYMENT_SERVICE_URL = 'http://localhost:5002'
-SHIPPING_SERVICE_URL = 'http://localhost:5003'
+INVENTORY_SERVICE_URL = 'http://127.0.0.1:5001'
+PAYMENT_SERVICE_URL = 'http://127.0.0.1:5002'
+SHIPPING_SERVICE_URL = 'http://127.0.0.1:5003'
 
 
 @app.route('/create_order', methods=['POST'])
@@ -18,17 +18,23 @@ def create_order():
         # Check inventory
         inventory_response = requests.post(f'{INVENTORY_SERVICE_URL}/reserve', json={'product_id': product_id,
                                                                                      'quantity': quantity})
-        inventory_response.raise_for_status()
+        # inventory_response.raise_for_status()
+        if inventory_response.status_code in (400, 500):
+            return jsonify(inventory_response.json()), inventory_response.status_code
 
         # Process payment
         payment_response = requests.post(f'{PAYMENT_SERVICE_URL}/pay',
                                          json={'amount': inventory_response.json()['total_price']})
-        payment_response.raise_for_status()
+        # payment_response.raise_for_status()
+        if payment_response.status_code in (400, 500):
+            return jsonify(payment_response.json()), payment_response.status_code
 
         # Ship order
         shipping_response = requests.post(f'{SHIPPING_SERVICE_URL}/ship', json={'product_id': product_id,
                                                                                 'quantity': quantity})
-        shipping_response.raise_for_status()
+        # shipping_response.raise_for_status()
+        if shipping_response.status_code in (400, 500):
+            return jsonify(shipping_response.json()), shipping_response.status_code
 
         return jsonify({"message": "Order processed successfully!"}), 200
     except requests.RequestException as e:
